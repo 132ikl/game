5cbcfbc2a2c43b6b5b6a730373594fce013d39e6
132ikl <132@ikl.sh>
2020-11-14 17:03:17 -0500

    Re-implement refactors from #1
    Co-authored-by: dzil123 <5725958+dzil123@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate rocket;
 mod database;
 
 use bcrypt;
-use database::database::{Database, Profile, UserData};
+use database::{Database, Profile, UserData};
 use rocket::http::Cookie;
 use rocket::http::Cookies;
 use rocket::request;
@@ -30,17 +30,11 @@ impl<'a, 'r> FromRequest<'a, 'r> for Profile {
     type Error = std::convert::Infallible;
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Profile, Self::Error> {
-        let db = Database::open();
-        let id_opt: Option<String> = request
+        match request
             .cookies()
             .get_private("user_id")
-            .and_then(|cookie| cookie.value().parse().ok());
-        let id = match id_opt {
-            Some(x) => x,
-            None => return Outcome::Forward(()),
-        };
-        let profile = db.load_profile(id);
-        match profile {
+            .and_then(|cookie| Database::open().load_profile(cookie.value()))
+        {
             Some(x) => Outcome::Success(x),
             None => Outcome::Forward(()),
         }
@@ -50,12 +44,16 @@ impl<'a, 'r> FromRequest<'a, 'r> for Profile {
 #[get("/")]
 fn index(mut profile: Profile, flash: Option<FlashMessage>) -> Template {
     let mut context = Context::new();
-    if let Some(x) = flash {
-        context.insert("message", x.msg());
+
+    // if profile.update() returns Some(msg), early exit, else get Option<msg> from flash
+    if let Some(msg) = profile
+        .update()
+        .as_deref()
+        .or_else(|| flash.as_ref().map(|item| item.msg()))
+    {
+        context.insert("message", msg);
     }
-    if let Some(msg) = profile.update() {
-        context.insert("message", &msg);
-    }
+
     context.insert("profile", &profile);
     Template::render("game", &context)
 }
@@ -65,63 +63,67 @@ fn index_redir() -> Redirect {
     Redirect::to("/login")
 }
 
-
 #[get("/get")]
 fn get(mut profile: Profile) -> Redirect {
-    let rd = Redirect::to("/");
-    if profile.update().is_some() {
-        return rd
+    if profile.update().is_none() {
+        profile.data.points += 1;
+        Database::open().save_profile(profile)
     };
-    profile.data.points = profile.data.points + 1;
-    let db = Database::open();
-    db.save_profile(profile);
-    rd
+    Redirect::to("/")
 }
 
 #[get("/login")]
 fn login_page(flash: Option<FlashMessage>) -> Template {
     let mut context = Context::new();
-    match flash {
-        Some(x) => context.insert("message", x.msg()),
-        None => {}
+
+    if let Some(x) = flash {
+        context.insert("message", x.msg())
     }
+
     Template::render("login", &context)
 }
 
 #[post("/register", data = "<form>")]
 fn register(form: Form<Login>) -> Flash<Redirect> {
+    if form.username.is_empty() || form.password.is_empty() {
+        return Flash::error(
+            Redirect::to("/login"),
+            "Username and password cannot be empty",
+        );
+    }
+
     let db = Database::open();
-    let id: Option<String> = db.get_id(form.username.clone());
-    if id.is_some() {
+
+    if db.from_username(&form.username).is_some() {
         return Flash::error(Redirect::to("/login"), "Account already exists");
     }
+
     let hash = bcrypt::hash(&form.password, 4).unwrap();
     let data = UserData::new(form.username.clone(), hash);
     let profile = Profile::new(db.gen_id().to_string(), data);
     db.save_profile(profile);
+
     Flash::success(Redirect::to("/login"), "Account creation successful")
 }
 
 #[post("/login", data = "<form>")]
 fn login(mut cookies: Cookies, form: Form<Login>) -> Result<Redirect, Flash<Redirect>> {
-    let err = Err(Flash::error(
-        Redirect::to("/login"),
-        "Incorrect username/password",
-    ));
-    let db = Database::open();
-    let id: Option<String> = db.get_id(form.username.clone());
-    let profile: Profile = match id {
-        Some(x) => match db.load_profile(x) {
-            Some(x) => x,
-            None => return err,
-        },
-        None => return err,
-    };
+    let err = || Flash::error(Redirect::to("/login"), "Incorrect username/password");
+
+    if form.username.is_empty() || form.password.is_empty() {
+        return Err(err());
+    }
+
+    let profile: Profile = Database::open()
+        .from_username(&form.username)
+        .ok_or(err())?;
+
     let success =
         bcrypt::verify(&form.password, &profile.data.hash).expect("Failed to verify password");
     if !success {
-        return err;
+        return Err(err());
     }
+
     cookies.add_private(Cookie::new("user_id", profile.id));
     Ok(Redirect::to("/"))
 }
@@ -134,10 +136,8 @@ fn logout(mut cookies: Cookies) -> Redirect {
 
 #[get("/leaderboard")]
 fn leaderboard(profile: Profile) -> Template {
-    let db = Database::open();
-    let mut sorted: Vec<_> = db
+    let mut sorted: Vec<_> = Database::open()
         .get_profiles()
-        .into_iter()
         .map(|profile| (profile.data.username, profile.data.points))
         .collect();
     sorted.sort_by(|a, b| b.1.cmp(&a.1));
