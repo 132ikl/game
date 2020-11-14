8057a4b636daa8598299f8774af2e17f93a4149e
dzil123 <5725958+dzil123@users.noreply.github.com>
2020-11-14 04:23:18 -0800

    refactor

--- src/database.rs
+++ src/database.rs
@@ -19,21 +19,20 @@ impl Profile {
     }
 
     pub fn update(&mut self) -> Option<String> {
-        let next =
-            DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(self.data.next, 0), Utc);
+        let next = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(self.data.next, 0), Utc);
         let now: DateTime<Utc> = Utc::now();
         if now > next {
             let next = (now + Duration::days(1)).timestamp();
             self.data.next = next;
             self.data.ready = true;
-            return None;
+            None
         } else {
             self.data.ready = false;
             let secs = (next - now).num_seconds();
             let h = secs / 3600;
             let rem = secs % 3600;
             let m = rem / 60;
-            return Some(format!("come back in {}h {}m to get again", h, m));
+            Some(format!("come back in {}h {}m to get again", h, m))
         }
     }
 }
@@ -43,7 +42,7 @@ pub struct UserData {
     pub username: String,
     pub hash: String,
     pub points: u16,
-    pub next: i64,
+    pub next: i64, // imo you should rewrite this to use native chrono serde serialize, but it will require db migration
     pub ready: bool,
     pub items: Vec<ShopItem>,
 }
@@ -101,41 +100,29 @@ impl Database {
             .expect("Failed to insert");
     }
 
-    pub fn load_profile(&self, id: String) -> Option<Profile> {
-        let vec: IVec = self.db.get(&id).unwrap()?;
-        let data: UserData = vec.into();
-        Some(Profile { id, data })
+    pub fn load_profile(&self, id: &str) -> Option<Profile> {
+        let vec = self.db.get(id).unwrap()?;
+        let data = vec.into();
+        Some(Profile::new(id.to_string(), data))
     }
 
-    pub fn get_profiles(&self) -> Vec<Profile> {
-        let mut profiles: Vec<Profile> = Vec::new();
-        for item in self.db.iter() {
-            profiles.push(match item.ok() {
-                Some(x) => {
-                    let vec: Vec<u8> = x.0.to_vec();
-                    let id: String = match from_utf8(&vec).ok() {
-                        Some(x) => x.to_owned(),
-                        None => continue,
-                    };
-                    let data: UserData = x.1.into();
-                    Profile { id, data }
-                }
-                None => continue,
-            });
-        }
-        profiles
+    pub fn get_profiles(&self) -> impl Iterator<Item = Profile> {
+        self.db
+            .iter()
+            .filter_map(|item| item.ok()) // skip if invalid row
+            .filter_map(|item| {
+                from_utf8(&item.0.to_vec()) // try to convert ivec to &str
+                    .map(|id| Profile::new(id.to_string(), item.1.into())) // if it worked, create profile
+                    .ok() // else skip row
+            })
     }
 
-    pub fn get_id(&self, username: String) -> Option<String> {
-        for profile in self.get_profiles() {
-            if profile.data.username == username {
-                return Some(profile.id);
-            }
-        }
-        None
+    pub fn get_by_username(&self, username: &str) -> Option<Profile> {
+        self.get_profiles()
+            .find(|profile| profile.data.username == username)
     }
 
-    pub fn gen_id(&self) -> u64 {
-        self.db.generate_id().unwrap()
+    pub fn gen_id(&self) -> String {
+        self.db.generate_id().unwrap().to_string()
     }
 }
